fff4447208c5c32990898bdc7f262d4aa99b923f
kirill <kirillulianow@gmail.com>
2023-06-17 00:35:42 +0300

    created initial components and frontend part structure

---
+++ Client/src/Pages/SignInForm/SignInForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignInForm = () => {
+    return (
+        <div>
+            SIGN IN FORM
+        </div>
+    );
+};
+
+export default SignInForm;
---
+++ Client/src/Pages/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpForm = () => {
+    return (
+        <div>
+            SIGN UP FORM
+        </div>
+    );
+};
+
+export default SignUpForm;
