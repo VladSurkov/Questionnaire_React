fe93f80c08e64461f22ba62ef473a17d4d1d7537
kirill <kirillulianow@gmail.com>
2023-06-17 18:55:15 +0300

    SignUp form created

--- Client/src/Pages/SignUpForm/SignUpForm.jsx
+++ Client/src/Pages/SignUpForm/SignUpForm.jsx
@@ -6,17 +6,17 @@ const SignUpForm = () => {
         <div className="SignUp">
 
             <div className="SignUp__container">
-                <h2 className="SignUp__title">Sign Up</h2>
-                <div className="SignUp__text">Stay with Questionire creating your personal account</div>
+                <h2 className="SignUp__title">Реєстрація</h2>
+                <div className="SignUp__text">Залишайтеся з <span>Questionaire</span> створивши свій обліковий запис.</div>
 
                 <form className="SignUp__form">
-                    <input type="text"  placeholder='Name'/>
-                    <input type="text"  placeholder='Surname'/>
-                    <input type="password"  placeholder='Password'/>
+                    <input type="text"  placeholder="Ім'я"/>
+                    <input type="text"  placeholder='Прізвище'/>
                     <input type="email"  placeholder='Email'/>
-
+                    <input type="password"  placeholder='Пароль'/>
+                    
                     <div className="SignUp__select">
-                        <label htmlFor="roles">Select your role:</label>
+                        <label htmlFor="roles">Оберіть роль:</label>
                         <select name="roles" id='roles'>
                             <option value="creator">Creator</option>
                             <option value="user">User</option>
@@ -24,7 +24,7 @@ const SignUpForm = () => {
                         </select>
                     </div>
 
-                    <button type="submit" class="SignUp__btn">Sign Up</button>
+                    <button type="submit" class="SignUp__btn">Зареєструватися</button>
 
                 </form>
 
